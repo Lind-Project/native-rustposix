8c6f9d9a0c18fc46b205f27eda4b04185fde543a
RusherRG <rushang101@gmail.com>
2024-04-11 00:08:55 +0000

    test: add check for RUSTPOSIX_PATH
    Signed-off-by: lind <lind@nyu.edu>

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+RUSTPOSIX_PATH="${RUSTPOSIX_PATH:-/home/lind/lind_project/src/safeposix-rust/target/release}"
+if [ ! -f $RUSTPOSIX_PATH/librustposix.so ]; then
+    echo "File not found: $RUSTPOSIX_PATH/librustposix.so"
+    exit 127
+fi
+
 echo "Copying librustposix.so"
-cp /home/lind/lind_project/src/safeposix-rust/target/release/librustposix.so librustposix.so
+cp $RUSTPOSIX_PATH/librustposix.so .
 
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 $CC $CFLAGS tests/lind_platform.c -o tests/lind_platform.o
